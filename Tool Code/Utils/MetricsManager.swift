397fbe4e3330081786fd10f6501f4493ddee840a
ac-bushrakhan <bushra.khan@armorcode.io>
2025-10-30 14:41:55 +0530

    Change SECRET_KEY in MetricsManager.swift
    Updated SECRET_KEY for testing purposes.

--- Tool Code/Utils/MetricsManager.swift
+++ Tool Code/Utils/MetricsManager.swift
@@ -1,7 +1,7 @@
 # Test file: Tool Code/Utils/MetricsManager.swift
 # This file contains test data for secret scanning
 
-SECRET_KEY="AKIAIOSFODNN7EXAMPLE"
+SECRET_KEY="AKIAIOSFODNN7C677993DDDFGMNJHEU"
 API_ENDPOINT="https://api.example.com/v1"
 
 # Additional test content
