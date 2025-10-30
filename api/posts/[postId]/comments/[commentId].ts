af981c5a0e210f966ac9664f6d6340d9007c3ab8
ac-PragatiDubey <pragati.dubey@armorcode.io>
2025-10-30 14:32:58 +0530

    Update [commentId].ts

--- api/posts/[postId]/comments/[commentId].ts
+++ api/posts/[postId]/comments/[commentId].ts
@@ -1,8 +1,25 @@
-# Test file: api/posts/[postId]/comments/[commentId].ts
-# This file contains test data for secret scanning
+# Save data to an AWS bucket
 
-SECRET_KEY="AKIAIOSFODNN7EXAMPLE"
-API_ENDPOINT="https://api.example.com/v1"
+from typing import Dict
 
-# Additional test content
-TEST_DATA="sample"
+import aws_lib
+import pymongo
+
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+
+def transform_data(es_data: Dict) -> Dict:
+    es_data = {**data, "origin": "ES"}
+
+MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
+
+def pull_data_from_mongo(query: Dict):
+    return pymongo.connect(MONGO_URI).fetch(query)
+
+
+def push_mongo_to_s3(query):
+    for element in pull_data_from_mongo(query):
+        upload(element)
